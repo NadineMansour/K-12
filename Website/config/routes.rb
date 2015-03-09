264f3db066b262700eeb965e8596dbb2cfcdce72
Mariam Mohamed Fawzy <maryammohamed61@gmail.com>
2015-03-09 21:49:09 +0200

    mariam : add part of Api in rails

--- Website/config/routes.rb
+++ Website/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+    namespace :api, defaults: { format: :json } do
+      resources :records do
+        collection do
+          post 'get_records_by_email'
+          post 'save_record'
+        end
+      end
+
+    end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
