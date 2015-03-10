823a0438fce7a8f4473995adf3c36ea12d4efb25
Apple <mido_2ofa@hotmail.com>
2015-03-10 20:56:30 +0200

    User sign up , user model , sign up views devise gem , validation for sign up fields

---
+++ Website/app/models/student.rb
@@ -0,0 +1,10 @@
+class Student < ActiveRecord::Base
+
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+ validates_presence_of :school , :grade , :student_class , :student_name 
+
+end
--- Website/db/schema.rb
+++ Website/db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309190114) do
+ActiveRecord::Schema.define(version: 20150310160919) do
+
+  create_table "installs", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "installs", ["email"], name: "index_installs_on_email", unique: true
+  add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
 
   create_table "records", force: :cascade do |t|
     t.string   "email"
@@ -22,5 +40,27 @@ ActiveRecord::Schema.define(version: 20150309190114) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "school"
+    t.string   "student_class"
+    t.string   "grade"
+    t.string   "student_name"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "students", ["email"], name: "index_students_on_email", unique: true
+  add_index "students", ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
 
 end
