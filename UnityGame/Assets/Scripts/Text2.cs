33934e6b906207949c740560a633a4e0068cee4b
MMelouk94 <mmelouk94@gmail.com>
2015-03-11 02:03:40 +0200

    Added more textures. Arranged and renamed some files

---
+++ UnityGame/Assets/Scripts/Text2.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Text2 : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+	public void SetText(string text) {
+		var myText = gameObject.GetComponent<Text>();
+		if (myText == null) {
+			myText = gameObject.AddComponent<Text>();
+		}
+		myText.text = text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (PlayerScript.isGameOver ()) {
+			SetText ("Congrats!! People of planet Saharawy thank you for enlightening their planet");
+		} 
+
+		
+	}
+}
