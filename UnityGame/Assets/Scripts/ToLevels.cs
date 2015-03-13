501ea0ac45ec50ac3d85d713ac15887c11ba7794
alisoliman <ali.soliman95@gmail.com>
2015-03-12 19:59:45 -0700

    Added checking levels from the database and assigning Locked text for the locked levels and directing to the end scene after finishing the game

--- UnityGame/Assets/Scripts/ToLevels.cs
+++ UnityGame/Assets/Scripts/ToLevels.cs
@@ -5,12 +5,24 @@ public class ToLevels : MonoBehaviour {
 
 	void Start () {
 	
+	
 	}
 
 	public void OnMouseDown() {
-		Application.LoadLevel("Level2");
+		if (Application.loadedLevel == 1) {
+			Application.LoadLevel(2);
+		}
+		if (Application.loadedLevel == 2) {
+			Application.LoadLevel(3);
+		}
+
+
+			
+			
+		
 	}
 
+	
 
 
 	// Update is called once per frame
